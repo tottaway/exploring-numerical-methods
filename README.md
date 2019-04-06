e1f818a0d1e53ff918d61782881b06af35b0fc8c
tottaway <tottaway123@gmail.com>
2019-04-05 20:06:34 -0400

    strange attractors

--- README.md
+++ README.md
@@ -6,7 +6,8 @@ I was inspired by my math class to explore various methods for computing numeric
 * Worked through examples on http://hplgit.github.io/num-methods-for-PDEs/doc/pub/diffu/sphinx/index.html and learned more about plotting using numpy and matplotlib.
 * Compared various method for solving ODE's numerically and computed their orders (Explicit Euler, Heun, Trapezoidal method, RK4)
 * Created a simulation of the two dimensional wave equation with options for changing initial conditions, adding periodic forces, and adjusting other various constants
-* Created a system to visualize arbitrary 2D systems of ODE's by animating a set of points as they flow through the vector field. Specifically built to understand the van der pol oscilator.
+* Created a system to visualize arbitrary 2D and 3D systems of ODE's by animating a set of points as they flow through the vector field. Specifically built to understand the van der pol oscilator.
+* Briefly explored strange attractors
 
 ### What's next:
 * Implementing a predictor-correct method for solving ordinary differential equations.
--- system_animator.py
+++ system_animator.py
@@ -73,7 +73,49 @@ def damped_pendulum(pos):
     return np.array([x_dot(pos[0], pos[1]), y_dot(pos[0], pos[1])])
 
 
-def run(time_chunks, time_chunk_size, system, dt, spawn_interval, spawn_batches):
+def strange_attractor(pos):
+    """Model of a pendulum"""
+
+    P = 10
+    R = 28
+    B = 8/3
+
+    def x_dot(x, y, z):
+        return P * (y - x)
+
+    def y_dot(x, y, z):
+        return R*x - y - x*z
+
+    def z_dot(x, y, z):
+        return x*y - B*z
+
+    return np.array([x_dot(pos[0], pos[1], pos[2]),
+                     y_dot(pos[0], pos[1], pos[2]),
+                     z_dot(pos[0], pos[1], pos[2])])
+
+    
+def rossler_attractor(pos):
+    """Model of a pendulum"""
+
+    A = 0.2
+    B = 0.2
+    C = 5.7
+
+    def x_dot(x, y, z):
+        return -(y + z)
+
+    def y_dot(x, y, z):
+        return x + A*y
+
+    def z_dot(x, y, z):
+        return B + x*z - C*z
+
+    return np.array([x_dot(pos[0], pos[1], pos[2]),
+                     y_dot(pos[0], pos[1], pos[2]),
+                     z_dot(pos[0], pos[1], pos[2])])
+
+
+def run(time_chunks, time_chunk_size, system, dt, spawn_interval, spawn_batches, three_d):
     """
     Compute 'time_chunks' frames of data incrementing each point
     'time_chunk_size' times per frame according to 'system'.
@@ -96,55 +138,94 @@ def run(time_chunks, time_chunk_size, system, dt, spawn_interval, spawn_batches)
 
             curr_state.append(point)
                  
+        x_values = np.linspace(-(X/2), (X/2), 10)
+        y_values = np.linspace(-(Y/2), (Y/2), 10)
+        z_values = np.linspace(-(Z/2), (Z/2), 10)
         if (t-1)%spawn_interval == 0 and t < spawn_batches *spawn_interval:
-            for i in range(-int(X/2), int(X/2)):
-                for j in range(-int(Y/2), int(Y/2)):
-                    pos = np.array([i, j])
-                    curr_state.append(Point(pos))
+            for i in x_values:
+                for j in y_values:
+                    if three_d:
+                        for k in z_values:
+                            pos = np.array([i, j, k])
+                            curr_state.append(Point(pos))
+                    else:
+                        pos = np.array([i, j])
+                        curr_state.append(Point(pos))
             
         state.append(curr_state)  
     return state
 
-def get_data(state):
+def get_data(state, three_d=False):
     """Get state to be plotted"""
-    global x, y
+    global x, y, z
     if not show_trails:
         x, y = [], []
+        if three_d: z = [] 
     for point in state:
         x.append(point.position[0])
         y.append(point.position[1])
-    return x, y
+        if three_d: z.append(point.position[2]) 
+    
+    if three_d:
+        return (x, y, z) 
+    else:
+        return (x, y)
 
 def update(t):
-    """Update function for animation"""
+    """update function for animation"""
     new_x, new_y = get_data(data[t])
     scat.set_data(new_x, new_y)
     return scat,
+
+
+def update_three_d(t):
+    """update function for animation"""
+    new_x, new_y, new_z = get_data(data[t], True)
+    scat.set_data((new_x, new_y))
+    scat.set_3d_properties(new_z)
+    return scat,
  
 if __name__ == "__main__":
-    T = 25
-    X, Y = 20, 10
+    T = 10
+    X, Y, Z = 40, 40, 40
 
     Nt = T * 50      # factor here should be inv. proporitional to epsilon
     time_chunks = int(T*25)                 # number of frames computed
     time_chunk_size = int(Nt/time_chunks)    # time steps to compute each frame 
 
-    time_dilation = 1/500 # view function on different time scales
+    time_dilation = 1/200 # view function on different time scales
     dt = time_dilation * T/time_chunk_size
     
 
 
-    fig, ax = plt.subplots()
-    plt.title("Van Der Pol")    
-    plt.xlabel("X")
-    plt.ylabel("Y")
-    plt.xlim(-X/2, X/2)
-    plt.ylim(-Y/2, Y/2)
-    show_trails = True
+    fig = plt.figure()
+    show_trails = False
      
-    data = run(time_chunks, time_chunk_size, pendulum, dt, 50, 1)
-    x, y = [], []
-    scat, = ax.plot(x, y, marker="o", ls="", color="black", alpha=0.25)
-    ani = FuncAnimation(fig, update, frames=time_chunks, interval=10)
+    three_d = True
+    data = run(time_chunks, time_chunk_size, strange_attractor, dt, 50, 1, three_d)
+
+    if three_d:
+        ax = fig.add_subplot(111, projection='3d')
+        
+        ax.set_xlabel("X")
+        ax.set_ylabel("Y")
+        ax.set_zlabel("Z")
+        ax.set_xlim(-X/2, X/2)
+        ax.set_ylim(-Y/2, Y/2)
+        ax.set_zlim(-Z/2, Z/2)
+
+        x, y, z = [], [], []
+        scat, = ax.plot(x, y, z, ls=" ", marker="o", color="black", alpha=0.5)
+        ani = FuncAnimation(fig, update_three_d, frames=time_chunks, interval=10)
+    else:
+        ax = fig.add_subplot(111, projection='2d')
+        plt.xlabel("X")
+        plt.ylabel("Y")
+        plt.xlim(-X/2, X/2)
+        plt.ylim(-Y/2, Y/2)
+        x, y = [], []
+        scat, = ax.plot(x, y, marker="o", ls="", color="black", alpha=0.25)
+        ani = FuncAnimation(fig, update, frames=time_chunks, interval=10)
+
     plt.show()
 
