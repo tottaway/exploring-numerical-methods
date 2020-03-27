0a5c2aa2b473352d89be951e06acfae73963cf5f
tottaway <tottaway123@gmail.com>
2020-03-26 20:29:32 -0400

    switch to pyqtgraph, way faster

--- two_d_wave.py
+++ two_d_wave.py
@@ -1,10 +1,14 @@
 import time
 import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib import cm
-from mpl_toolkits.mplot3d import Axes3D
-from matplotlib.animation import FuncAnimation
+import sys
+# import matplotlib.pyplot as plt
+# from matplotlib import cm
+# from mpl_toolkits.mplot3d import Axes3D
+# from matplotlib.animation import FuncAnimation
 from tqdm import tqdm
+from pyqtgraph.Qt import QtCore, QtGui
+import pyqtgraph.opengl as gl
+import pyqtgraph as pg
 """
 About this file:
 Runs a simulation of the wave equation in two dimensions
@@ -18,14 +22,14 @@ Things to explore:
 * Raise and lower Nt to examine stabilty of solutions
 """
 c = 0.005
-T = 45
-X, Y = 4, 4
+T = 100
+X, Y = 8, 8
 
-Nt = 3500
-time_chunks = int(T*2)                 # number of frames computed
+Nt = 10*T
+time_chunks = int(T)                 # number of frames computed
 time_chunk_size = int(Nt/time_chunks)   # time steps to compute each frame
-Nx = X * 35 
-Ny = Y * 35
+Nx = X * 10 
+Ny = Y * 10
 
 dt = T/time_chunk_size
 dx = X / Nx
@@ -33,9 +37,10 @@ dy = Y / Ny
 
 X_values = np.arange(-X/2, X/2, dx)
 Y_values = np.arange(-Y/2, Y/2, dy)
-X_values, Y_values = np.meshgrid(X_values, Y_values)
+tmp_x_values, tmp_y_values = np.meshgrid(X_values, Y_values)
 
-initial_cond = np.zeros((Nx, Ny)) #np.sin(10 / ((np.sqrt(X_values**2 + Y_values**2) + 1) - 0.5))
+# initial_cond = np.zeros((Nx, Ny))
+initial_cond = np.sin(10 / ((np.sqrt(tmp_x_values**2 + tmp_y_values**2) + 1) - 0.5))
 
 hold_boundary_at_zero = True
 
@@ -43,7 +48,7 @@ hold_boundary_at_zero = True
 def Utt(Uxx, Uyy, t):
     """Wave equation: set time dependent forces using 'force' matrix"""
     force = np.zeros((Nx, Ny))
-    force[int(3*Nx/4), int(3*Ny/4)] = 0.1*np.cos(t)
+    # force[int(3*Nx/4), int(3*Ny/4)] = 0.003*np.cos(0.5*t)
     return c**2 * (Uxx + Uyy) + force
 
 def calc_Uxx(U_1):
@@ -105,24 +110,74 @@ def wave(Nt, Nx, Ny, U0, boundary):
         U[time_chunk, :, :] = U_curr[-1, :, :]
         Ut[time_chunk, :, :] = Ut_curr[-1, :, :]
 
+    
+    app = pg.mkQApp()
 
-    # create animation
-    fig = plt.figure()
+    view = gl.GLViewWidget()
+    view.show()
 
-    plt.title("Animation of wave equation")
-    ax = fig.add_subplot(111, projection='3d')
-    zs = U[0, :, :]
-    ax.set_zlim(-4, 4)
-    surface = None
-    tstart = time.time()
-    for t in range(0, time_chunks):
-        if surface:
-            ax.collections.remove(surface)
+    xgrid = gl.GLGridItem()
+    ygrid = gl.GLGridItem()
+    zgrid = gl.GLGridItem()
 
-        Z = U[t, :, :]
-        surface = ax.plot_surface(X_values, Y_values, Z, cmap="Blues", linewidth=10,
-          antialiased=False)
-        plt.pause(.0001)
+    xgrid.rotate(90, 0, 1, 0)
+    ygrid.rotate(90, 1, 0, 0)
+    
+
+    colors = np.ones((Nx, Ny, 4)) * U[0, :, :, np.newaxis]
+    min_u = np.min(U)
+    max_u = np.max(U) - min_u
+    colors = (colors - min_u) / max_u
+
+    surface = gl.GLSurfacePlotItem(
+            x=X_values,
+            y=Y_values,
+            z=U[0, :, :],
+            colors=colors
+            )
+    view.addItem(surface)
+
+    view.pan(50, 50, 60)
+
+    t = 0
+    def update():
+        nonlocal t
+        colors = np.ones((Nx, Ny, 4)) * U[t, :, :, np.newaxis]
+        colors = (colors - min_u) / max_u
+        surface.setData(x=X_values,
+                y=Y_values,
+                z=U[t, :, :],
+                colors=colors
+                ) 
+        t += 1
+        t %= U.shape[0]
+
+    timer = QtCore.QTimer()
+    timer.timeout.connect(update)
+    timer.start(30)
+
+    view.pan(-50, -50, -60)
+    print(view.cameraPosition())
+
+    if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
+        app.exec_()  # Start QApplication event loop ***
+    # # create animation
+    # fig = plt.figure()
+
+    # plt.title("Animation of wave equation")
+    # ax = fig.add_subplot(111, projection='3d')
+    # zs = U[0, :, :]
+    # ax.set_zlim(-4, 4)
+    # surface = None
+    # tstart = time.time()
+    # for t in range(0, time_chunks):
+    #     if surface:
+    #         ax.collections.remove(surface)
+
+    #     Z = U[t, :, :]
+    #     surface = ax.plot_surface(X_values, Y_values, Z, cmap="Blues", linewidth=10,
+    #       antialiased=False)
+    #     plt.pause(.0001)
 
 
 if __name__=="__main__":
